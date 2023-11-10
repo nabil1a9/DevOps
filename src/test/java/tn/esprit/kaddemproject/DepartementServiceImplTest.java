3f5f09028710ab7bbc0d1c62fa09898284e603d0
omayma2020 <75322883+omayma2020@users.noreply.github.com>
2023-11-10 13:15:17 +0100

    Test adjustment

--- src/test/java/tn/esprit/kaddemproject/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/kaddemproject/DepartementServiceImplTest.java
@@ -10,6 +10,9 @@ import tn.esprit.kaddemproject.repositories.DepartementRepository;
 import tn.esprit.kaddemproject.services.IDepartementServiceImpl;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -36,6 +39,29 @@ public class DepartementServiceImplTest {
         Departement result = departementService.addDepartement(dep);
         assertEquals(dep, result);
     }
+    @Test
+    void retrieveAllDepartments() {
+        List<Departement> departments = new ArrayList<>();
+        when(departementRepository.findAll()).thenReturn(departments);
+        List<Departement> result = departementService.retrieveAllDepartements();
+        assertEquals(departments, result);
+    }
+    @Test
+    void deleteDepartment() {
+        int id = 1;
+        doNothing().when(departementRepository).deleteById(id);
+        departementService.deleteDepartement(id);
+        verify(departementRepository, times(1)).deleteById(id);
+    }
+    @Test
+    void retrieveDepartment() {
+        int id = 1;
+        Departement dep = new Departement();
+        when(departementRepository.findById(id)).thenReturn(Optional.of(dep));
+        Departement result = departementService.retrieveDepartement(id);
+        assertEquals(dep, result);
+    }
+
 
 
 }
